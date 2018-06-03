996a432a40c2c17e19025875bf085b997bd22118
Niklas Mollenhauer <nikeee@outlook.com>
2018-06-03 13:17:32 +0200

    Fix compiler errors
    See:
    https://github.com/Microsoft/TypeScript/issues/12215#issuecomment-377567046

--- src/types.ts
+++ src/types.ts
@@ -495,8 +495,7 @@ export interface TypeSymbol {
 	members?: SymbolTable; // sub-types (when current syntax node is a NodeStatement, members are the attribute left IDs)
 }
 
-export type Diff<T extends string, U extends string> = ({[P in T]: P } & {[P in U]: never } & { [x: string]: never })[T];
-export type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>;
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
 
 export type StatementOf<T extends Statement["kind"]> = T extends SyntaxKind.SubGraphStatement ? SubGraphStatement
 	: T extends SyntaxKind.AttributeStatement ? AttributeStatement
