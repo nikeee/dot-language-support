fcac8b79054dd3bd5382ad87c86598ba45acef99
Niklas Mollenhauer <nikeee@outlook.com>
2018-06-03 13:05:08 +0200

    Add test for code action offering

--- tests/command/removeSemicolon.spec.ts
+++ tests/command/removeSemicolon.spec.ts
@@ -6,9 +6,9 @@ import "mocha";
 
 import * as RemoveSemicolons from "../../src/service/command/RemoveSemicolons";
 import { TextDocument } from "vscode-languageserver-types/lib/umd/main";
-import { CommandIds } from "../../src/service/codeAction";
+import { CommandIds, getCodeActions } from "../../src/service/codeAction";
 
-describe("Command execution", () => {
+describe("Remove semicolon command execution", () => {
 
 	it("should get correct semicolon edits", () => {
 		const content = `strict digraph {
@@ -38,26 +38,81 @@ describe("Command execution", () => {
 
 		const command = {
 			command: CommandIds.RemoveSemicolons,
-			arguments: undefined
+			arguments: undefined,
 		};
 
 		const execution = RemoveSemicolons.execute(doc, sf, command);
 
 		expect(execution).to.exist;
-		if(!execution) throw "Just for the type checker";
+		if (!execution) throw "Just for the type checker";
 
 
 		expect(execution.edit.changes).to.exist;
-		if(!execution.edit.changes) throw "Just for the type checker";
+		if (!execution.edit.changes) throw "Just for the type checker";
 
 		const edits = execution.edit.changes[doc.uri];
 		expect(edits).to.exist;
-		if(!edits) throw "Just for the type checker";
+		if (!edits) throw "Just for the type checker";
 
 		expect(edits).to.have.length(semicolons);
 
 		const actual = TextDocument.applyEdits(doc, edits);
 
 		expect(actual).to.equal(expected);
+	});
+
+	it("should offer code action", () => {
+		const content = `strict digraph {
+			a -> b;
+			a -> C
+			a -> d;
+			a -> e [color=red];
+			a -> f [color=red]
+			a -> { g h i };
+			a -> { g; { g; h -> c; i }; i }
+			a -> { g h i } [color=blue];
+		}`;
+
+		const [doc, sf] = ensureDocAndSourceFile(content);
+		const pg = ensureGraph(sf);
+
+		const secondSemicolon = pg.statements[2].terminator;
+		expect(secondSemicolon).to.exist;
+		if (!secondSemicolon) throw "Just for the type checker";
+
+		const start = secondSemicolon.end - 1;
+		const end = secondSemicolon.end;
+
+		const range = {
+			start: doc.positionAt(start),
+			end: doc.positionAt(start),
+		};
+
+		let actions = getCodeActions(doc, sf, range, undefined);
+		expect(actions).to.exist;
+		if (!actions) throw "Just for the type checker";
+
+		let firstAction = actions[0];
+		expect(firstAction).to.exist;
+		if (!firstAction) throw "Just for the type checker";
+
+		expect(firstAction.command).to.equal(CommandIds.RemoveSemicolons);
+		expect(firstAction.arguments).to.be.undefined;
+		expect(firstAction.title).to.exist;
+/*
+		range.start = doc.positionAt(end);
+		range.end = doc.positionAt(end);
+		actions = getCodeActions(doc, sf, range, undefined);
+		expect(actions).to.exist;
+		if (!actions) throw "Just for the type checker";
+
+		firstAction = actions[0];
+		expect(firstAction).to.exist;
+		if (!firstAction) throw "Just for the type checker";
+
+		expect(firstAction.command).to.equal(CommandIds.RemoveSemicolons);
+		expect(firstAction.arguments).to.be.undefined;
+		expect(firstAction.title).to.exist;
+*/
 	});
 });
--- tests/testutils.ts
+++ tests/testutils.ts
@@ -2,6 +2,8 @@ import { DefaultScanner, Parser } from "../src"
 import { SourceFile, Graph } from "../src/types";
 import { expect } from "chai";
 import { TextDocument } from "vscode-languageserver-types/lib/umd/main";
+import { bindSourceFile } from "../src/binder";
+import { checkSourceFile } from "../src/checker";
 
 export function createParserWithText(text: string) {
 	const p = new Parser();
@@ -21,6 +23,7 @@ export function ensureGraph(sourceFile: SourceFile): Graph {
 export function ensureDocAndSourceFile(text: string): [TextDocument, SourceFile]{
 	const doc = TextDocument.create("inmemory://model.json", "DOT", 0, text);
 	const p = createParserWithText(doc.getText());
-
+	bindSourceFile(p);
+	checkSourceFile(p);
 	return [doc, p];
 }
