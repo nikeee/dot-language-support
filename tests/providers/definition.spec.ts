f0ea6e887771ad7d0e7148a282f685ff58e99990
Niklas Mollenhauer <nikeee@outlook.com>
2018-06-16 02:15:31 +0200

    Add test for definition finding

---
+++ tests/providers/definition.spec.ts
@@ -0,0 +1,49 @@
+import { ensureDocAndSourceFile } from "../testutils";
+import { expect } from "chai";
+import "mocha";
+import { findDefinition } from "../../src/service/reference";
+
+describe("Definition Finding", () => {
+
+	function findDefinitionSample(content: string) {
+		return (offset: number) => {
+			const [doc, sf] = ensureDocAndSourceFile(content);
+			const refs = findDefinition(doc, sf, doc.positionAt(offset));
+
+			expect(refs).to.exist;
+			if (!refs) throw "Just for the type checker";
+
+			return refs;
+		}
+	}
+
+	it("should correctly find reference (referring other)", () => {
+		const as = findDefinitionSample(`graph {a a b a}`);
+
+		const def = as(9);
+		expect(def).to.exist;
+		expect(def.range).to.exist;
+		expect(def.range.start).to.exist;
+		expect(def.range.end).to.exist;
+
+		expect(def.range.start.character).to.equal(7);
+		expect(def.range.end.character).to.equal(8);
+		expect(def.range.start.line).to.equal(0);
+		expect(def.range.end.line).to.equal(0);
+	});
+
+	it("should correctly find reference (referring other)", () => {
+		const as = findDefinitionSample(`graph {a a b a}`);
+
+		const def = as(7);
+		expect(def).to.exist;
+		expect(def.range).to.exist;
+		expect(def.range.start).to.exist;
+		expect(def.range.end).to.exist;
+
+		expect(def.range.start.character).to.equal(7);
+		expect(def.range.end.character).to.equal(8);
+		expect(def.range.start.line).to.equal(0);
+		expect(def.range.end.line).to.equal(0);
+	});
+});
