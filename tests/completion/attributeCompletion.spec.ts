19292dd5a96b6a4125818be94496dfd424338e7c
Niklas Mollenhauer <nikeee@outlook.com>
2018-10-19 16:29:47 +0200

    Add assertions for multi-container attributes

--- tests/completion/attributeCompletion.spec.ts
+++ tests/completion/attributeCompletion.spec.ts
@@ -109,4 +109,98 @@ describe("Attribute completion", () => {
 		expect(completions.map(getLabel)).not.to.contain("node_name_a");
 		expect(completions.map(getLabel)).not.to.contain("node_name_b");
 	});
+
+
+
+	it("should provide completion for attributes (empty list, first container)", () => {
+		const content = `graph {
+			node_name_a -- node_name_b [] [shape=box];
+		}`;
+		const requestOffset = invokeIndex(content)("[");
+
+		const [doc, sf] = ensureDocAndSourceFile(content);
+
+		const completions = getCompletions(doc, sf, doc.positionAt(requestOffset));
+
+		expect(completions).to.exist;
+		if (!completions) throw "Just for the type checker";
+
+		expect(completions).to.have.length.greaterThan(0);
+		expect(completions.map(getLabel)).not.to.contain("node_name_a");
+		expect(completions.map(getLabel)).not.to.contain("node_name_b");
+	});
+
+	it("should provide completion for attributes (empty list, a lot of whitespace, first container)", () => {
+		const content = `graph {
+			node_name_a -- node_name_b [    ] [shape=box];
+		}`;
+
+		const requestOffset = invokeIndex(content)("[  ");
+
+		const [doc, sf] = ensureDocAndSourceFile(content);
+
+		const completions = getCompletions(doc, sf, doc.positionAt(requestOffset));
+
+		expect(completions).to.exist;
+		if (!completions) throw "Just for the type checker";
+
+		expect(completions).to.have.length.greaterThan(0);
+		expect(completions.map(getLabel)).not.to.contain("node_name_a");
+		expect(completions.map(getLabel)).not.to.contain("node_name_b");
+	});
+
+	it("should provide completion for attributes (preceeding item, first container)", () => {
+		const content = `graph {
+			node_name_a -- node_name_b [color=blue,] [shape=box];
+		}`;
+		const requestOffset = invokeIndex(content)("color=blue,");
+
+		const [doc, sf] = ensureDocAndSourceFile(content);
+
+		const completions = getCompletions(doc, sf, doc.positionAt(requestOffset));
+
+		expect(completions).to.exist;
+		if (!completions) throw "Just for the type checker";
+
+		expect(completions).to.have.length.greaterThan(0);
+		expect(completions.map(getLabel)).not.to.contain("node_name_a");
+		expect(completions.map(getLabel)).not.to.contain("node_name_b");
+	});
+
+	it("should provide completion for attributes (preceeding item, leading whitespace, first container)", () => {
+		const content = `graph {
+			node_name_a -- node_name_b [color=blue, ] [shape=box];
+		}`;
+		const requestOffset = invokeIndex(content)("color=blue, ");
+
+		const [doc, sf] = ensureDocAndSourceFile(content);
+
+		const completions = getCompletions(doc, sf, doc.positionAt(requestOffset));
+
+		expect(completions).to.exist;
+		if (!completions) throw "Just for the type checker";
+
+		expect(completions).to.have.length.greaterThan(0);
+		expect(completions.map(getLabel)).not.to.contain("node_name_a");
+		expect(completions.map(getLabel)).not.to.contain("node_name_b");
+	});
+
+	it("should provide completion for attributes (preceeding item, leading whitespace, linebreak, first container)", () => {
+		const content = `graph {
+			node_name_a -- node_name_b [color=blue,
+			] [shape=box];
+		}`;
+		const requestOffset = invokeIndex(content)("color=blue, \n\t\t\t");
+
+		const [doc, sf] = ensureDocAndSourceFile(content);
+
+		const completions = getCompletions(doc, sf, doc.positionAt(requestOffset));
+
+		expect(completions).to.exist;
+		if (!completions) throw "Just for the type checker";
+
+		expect(completions).to.have.length.greaterThan(0);
+		expect(completions.map(getLabel)).not.to.contain("node_name_a");
+		expect(completions.map(getLabel)).not.to.contain("node_name_b");
+	});
 });
