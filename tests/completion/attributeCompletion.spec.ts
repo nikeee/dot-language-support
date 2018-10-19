d228f982455f58141270d110064ab3163c0e524d
Niklas Mollenhauer <nikeee@outlook.com>
2018-10-19 16:24:34 +0200

    Enhance tests

--- tests/completion/attributeCompletion.spec.ts
+++ tests/completion/attributeCompletion.spec.ts
@@ -10,12 +10,38 @@ function getLabel(c: CompletionItem) {
 }
 
 describe("Attribute completion", () => {
+	function invokeIndex(content: string) {
+		return (location: string) => {
+			const requestOffset = content.indexOf(location) + location.length;
+			expect(requestOffset).greaterThan(-1);
+			return requestOffset;
+		}
+	}
+
 	it("should provide completion for attributes (empty list)", () => {
 		const content = `graph {
 			node_name_a -- node_name_b [];
 		}`;
-		const requestOffset = content.indexOf("[") + "[".length;
-		expect(requestOffset).greaterThan(-1);
+		const requestOffset = invokeIndex(content)("[");
+
+		const [doc, sf] = ensureDocAndSourceFile(content);
+
+		const completions = getCompletions(doc, sf, doc.positionAt(requestOffset));
+
+		expect(completions).to.exist;
+		if (!completions) throw "Just for the type checker";
+
+		expect(completions).to.have.length.greaterThan(0);
+		expect(completions.map(getLabel)).not.to.contain("node_name_a");
+		expect(completions.map(getLabel)).not.to.contain("node_name_b");
+	});
+
+	it("should provide completion for attributes (empty list, a lot of whitespace)", () => {
+		const content = `graph {
+			node_name_a -- node_name_b [    ];
+		}`;
+
+		const requestOffset = invokeIndex(content)("[  ");
 
 		const [doc, sf] = ensureDocAndSourceFile(content);
 
@@ -33,8 +59,7 @@ describe("Attribute completion", () => {
 		const content = `graph {
 			node_name_a -- node_name_b [color=blue,];
 		}`;
-		const requestOffset = content.indexOf("color=blue,") + "color=blue,".length;
-		expect(requestOffset).greaterThan(-1);
+		const requestOffset = invokeIndex(content)("color=blue,");
 
 		const [doc, sf] = ensureDocAndSourceFile(content);
 
@@ -52,8 +77,7 @@ describe("Attribute completion", () => {
 		const content = `graph {
 			node_name_a -- node_name_b [color=blue, ];
 		}`;
-		const requestOffset = content.indexOf("color=blue, ") + "color=blue, ".length;
-		expect(requestOffset).greaterThan(-1);
+		const requestOffset = invokeIndex(content)("color=blue, ");
 
 		const [doc, sf] = ensureDocAndSourceFile(content);
 
@@ -72,8 +96,7 @@ describe("Attribute completion", () => {
 			node_name_a -- node_name_b [color=blue,
 			];
 		}`;
-		const requestOffset = content.indexOf("color=blue, \n\t\t\t") + "color=blue, \n\t\t\t".length;
-		expect(requestOffset).greaterThan(-1);
+		const requestOffset = invokeIndex(content)("color=blue, \n\t\t\t");
 
 		const [doc, sf] = ensureDocAndSourceFile(content);
 
