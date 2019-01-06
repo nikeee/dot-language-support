74408eb25bfcc5bd87a444ada4ce5b165ab6d08a
Niklas Mollenhauer <nikeee@outlook.com>
2019-01-06 01:01:39 +0100

    Add Tests for #17

--- tests/completion/nodeCompletion.spec.ts
+++ tests/completion/nodeCompletion.spec.ts
@@ -61,4 +61,140 @@ describe("Node completion", () => {
 
 		expect(completions).to.have.length(4); // green, blue, yellow, b
 	});
+
+	it("should provide completion for nodes (trailing space; between nodes, undirected)", () => {
+		const content = `graph{node_0;node_1;node_2;node_0 -- node_1 -- [color=gray]; }`;
+		const requestOffset = content.indexOf("node_1 --") + "node_1 --".length;
+
+		const [doc, sf] = ensureDocAndSourceFile(content);
+
+		const completions = getCompletions(doc, sf, doc.positionAt(requestOffset));
+
+		expect(completions).to.exist;
+		if (!completions) throw "Just for the type checker";
+
+		expect(completions).to.have.length(3); // node_{0,1,2}
+	});
+
+	it("should provide completion for nodes (trailing space; between nodes, directed)", () => {
+		const content = `digraph{node_0;node_1;node_2;node_0 -> node_1 -> [color=gray]; }`;
+		const requestOffset = content.indexOf("node_1 ->") + "node_1 ->".length;
+
+		const [doc, sf] = ensureDocAndSourceFile(content);
+
+		const completions = getCompletions(doc, sf, doc.positionAt(requestOffset));
+
+		expect(completions).to.exist;
+		if (!completions) throw "Just for the type checker";
+
+		expect(completions).to.have.length(3); // node_{0,1,2}
+	});
+
+	it("should provide completion for nodes (trailing space; between nodes, undirected)", () => {
+		const content = `graph{node_0;node_1;node_2;node_0 -- node_1 -- [color=gray]; }`;
+		const requestOffset = content.indexOf("node_1 -- ") + "node_1 -".length;
+
+		const [doc, sf] = ensureDocAndSourceFile(content);
+
+		const completions = getCompletions(doc, sf, doc.positionAt(requestOffset));
+
+		expect(completions).to.exist;
+		if (!completions) throw "Just for the type checker";
+
+		expect(completions).to.have.length(3); // node_{0,1,2}
+	});
+
+	it("should provide completion for nodes (trailing space; between nodes, directed)", () => {
+		const content = `digraph{node_0;node_1;node_2;node_0 -> node_1 -> [color=gray]; }`;
+		const requestOffset = content.indexOf("node_1 -> ") + "node_1 -".length;
+
+		const [doc, sf] = ensureDocAndSourceFile(content);
+
+		const completions = getCompletions(doc, sf, doc.positionAt(requestOffset));
+
+		expect(completions).to.exist;
+		if (!completions) throw "Just for the type checker";
+
+		expect(completions).to.have.length(3); // node_{0,1,2}
+	});
+
+	it("should provide completion for nodes (trailing space; between nodes, issue #17 1)", () => {
+		const content = `digraph {
+	music_bus [label="Which music would you like to listen to on the bus?"]
+	breakfast [label="What breakfast do you want to eat?"]
+	sell_game [label="Do you want to program bandersnatch at the company?"]
+
+	music_bus -> breakfast -> [color=gray]
+}`;
+		const requestOffset = content.indexOf("breakfast ->") + "breakfast ->".length;
+
+		const [doc, sf] = ensureDocAndSourceFile(content);
+
+		const completions = getCompletions(doc, sf, doc.positionAt(requestOffset));
+
+		expect(completions).to.exist;
+		if (!completions) throw "Just for the type checker";
+
+		expect(completions).to.have.length(3); // music_bus, breakfast, sell_game
+	});
+
+	it("should provide completion for nodes (trailing space; between nodes, issue #17 2)", () => {
+		const content = `digraph {
+	music_bus [label="Which music would you like to listen to on the bus?"]
+	breakfast [label="What breakfast do you want to eat?"]
+	sell_game [label="Do you want to program bandersnatch at the company?"]
+
+	music_bus -> breakfast ->  [color=gray]
+}`;
+		const requestOffset = content.indexOf("breakfast ->") + "breakfast ->".length;
+
+		const [doc, sf] = ensureDocAndSourceFile(content);
+
+		const completions = getCompletions(doc, sf, doc.positionAt(requestOffset));
+
+		expect(completions).to.exist;
+		if (!completions) throw "Just for the type checker";
+
+		expect(completions).to.have.length(3); // music_bus, breakfast, sell_game
+	});
+
+	it("should provide completion for nodes (trailing space; between nodes, issue #17 3)", () => {
+		const content = `digraph {
+	music_bus [label="Which music would you like to listen to on the bus?"]
+	breakfast [label="What breakfast do you want to eat?"]
+	sell_game [label="Do you want to program bandersnatch at the company?"]
+
+	music_bus -> breakfast ->[color=gray]
+}`;
+		const requestOffset = content.indexOf("breakfast ->") + "breakfast ->".length;
+
+		const [doc, sf] = ensureDocAndSourceFile(content);
+
+		const completions = getCompletions(doc, sf, doc.positionAt(requestOffset));
+
+		expect(completions).to.exist;
+		if (!completions) throw "Just for the type checker";
+
+		expect(completions).to.have.length(3); // music_bus, breakfast, sell_game
+	});
+
+	it("should provide completion for nodes (trailing space; between nodes, issue #17 4)", () => {
+		const content = `digraph {
+	music_bus [label="Which music would you like to listen to on the bus?"]
+	breakfast [label="What breakfast do you want to eat?"]
+	sell_game [label="Do you want to program bandersnatch at the company?"]
+
+	music_bus -> breakfast ->[color=gray,]
+}`;
+		const requestOffset = content.indexOf("color=gray,") + "color=gray,".length;
+
+		const [doc, sf] = ensureDocAndSourceFile(content);
+
+		const completions = getCompletions(doc, sf, doc.positionAt(requestOffset));
+
+		expect(completions).to.exist;
+		if (!completions) throw "Just for the type checker";
+
+		expect(completions).to.have.length(170); // music_bus, breakfast, sell_game
+	});
 });
