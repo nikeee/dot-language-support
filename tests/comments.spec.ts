ee928ca5b4657d1ded74ad7430d913a1496d9643
Niklas Mollenhauer <nikeee@outlook.com>
2026-01-13 19:19:17 +0100

    Simplify assertions

--- tests/comments.spec.ts
+++ tests/comments.spec.ts
@@ -13,69 +13,45 @@ describe("Comment Handling", () => {
 		}`);
 		const pg = ensureGraph(p);
 
-		expect(pg.strict).toBeFalsy();
-		expect(pg.kind).toEqual(syntaxKind.DirectedGraph);
-		if (pg.kind !== syntaxKind.DirectedGraph) throw "Just for type checker";
+		expect(pg).toMatchObject({
+			strict: undefined,
+			kind: syntaxKind.DirectedGraph,
+			id: expect.objectContaining({ text: "G" }),
+		});
 
-		// expect(pg.openBrace).to.exist;
-		// expect(pg.closeBrace).to.exist;
-		// biome-ignore lint/suspicious/noExplicitAny: :shrug:
-		expect((pg.id as any).text).toEqual("G");
+		const { statements } = pg;
+		expect(statements).toHaveLength(4);
 
-		const s = pg.statements;
-		expect(s).toBeDefined();
-		expect(s.length).toEqual(4);
+		expect(statements[0]).toMatchObject({
+			kind: syntaxKind.IdEqualsIdStatement,
+			terminator: expect.objectContaining({
+				kind: syntaxKind.SemicolonToken,
+			}),
+			leftId: expect.objectContaining({ text: "a" }),
+			rightId: expect.objectContaining({ text: "b" }),
+		});
 
-		const s0 = s[0];
-		expect(s0).toBeDefined();
-		expect(s0.kind).toEqual(syntaxKind.IdEqualsIdStatement);
-		if (s0.kind !== syntaxKind.IdEqualsIdStatement) throw "Just for type checker";
-		expect(s0.terminator).toBeDefined();
-		expect(s0.leftId).toBeDefined();
-		// biome-ignore lint/suspicious/noExplicitAny: :shrug:
-		expect((s0.leftId as any).text).toEqual("a");
-		// expect(s0.equalsToken).to.exist;
-		expect(s0.rightId).toBeDefined();
-		// biome-ignore lint/suspicious/noExplicitAny: :shrug:
-		expect((s0.rightId as any).text).toEqual("b");
+		expect(statements[1]).toMatchObject({
+			kind: syntaxKind.IdEqualsIdStatement,
+			terminator: undefined,
+			leftId: expect.objectContaining({ text: "c" }),
+			rightId: expect.objectContaining({ text: "d" }),
+		});
 
-		const s1 = s[1];
-		expect(s1).toBeDefined();
-		expect(s1.kind).toEqual(syntaxKind.IdEqualsIdStatement);
-		if (s1.kind !== syntaxKind.IdEqualsIdStatement) throw "Just for type checker";
-		expect(s1.terminator).toBeFalsy();
-		expect(s1.leftId).toBeDefined();
-		// biome-ignore lint/suspicious/noExplicitAny: :shrug:
-		expect((s1.leftId as any).text).toEqual("c");
-		// expect(s1.equalsToken).to.exist;
-		expect(s1.rightId).toBeDefined();
-		// biome-ignore lint/suspicious/noExplicitAny: :shrug:
-		expect((s1.rightId as any).text).toEqual("d");
+		expect(statements[2]).toMatchObject({
+			kind: syntaxKind.IdEqualsIdStatement,
+			terminator: undefined,
+			leftId: expect.objectContaining({ text: "e" }),
+			rightId: expect.objectContaining({ text: "f" }),
+		});
 
-		const s2 = s[2];
-		expect(s2).toBeDefined();
-		expect(s2.kind).toEqual(syntaxKind.IdEqualsIdStatement);
-		if (s2.kind !== syntaxKind.IdEqualsIdStatement) throw "Just for type checker";
-		expect(s2.terminator).toBeFalsy();
-		expect(s2.leftId).toBeDefined();
-		// biome-ignore lint/suspicious/noExplicitAny: :shrug:
-		expect((s2.leftId as any).text).toEqual("e");
-		// expect(s2.equalsToken).to.exist;
-		expect(s2.rightId).toBeDefined();
-		// biome-ignore lint/suspicious/noExplicitAny: :shrug:
-		expect((s2.rightId as any).text).toEqual("f");
-
-		const s3 = s[3];
-		expect(s3).toBeDefined();
-		expect(s3.kind).toEqual(syntaxKind.IdEqualsIdStatement);
-		if (s3.kind !== syntaxKind.IdEqualsIdStatement) throw "Just for type checker";
-		expect(s3.terminator).toBeDefined();
-		expect(s3.leftId).toBeDefined();
-		// biome-ignore lint/suspicious/noExplicitAny: :shrug:
-		expect((s3.leftId as any).text).toEqual("g");
-		// expect(s3.equalsToken).to.exist;
-		expect(s3.rightId).toBeDefined();
-		// biome-ignore lint/suspicious/noExplicitAny: :shrug:
-		expect((s3.rightId as any).text).toEqual("3");
+		expect(statements[3]).toMatchObject({
+			kind: syntaxKind.IdEqualsIdStatement,
+			terminator: expect.objectContaining({
+				kind: syntaxKind.SemicolonToken,
+			}),
+			leftId: expect.objectContaining({ text: "g" }),
+			rightId: expect.objectContaining({ text: "3" }),
+		});
 	});
 });
